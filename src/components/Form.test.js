d820f5ea268e7699f37c5cb5ca8baa10e1b06f3b
Felipe César <felipecesr@gmail.com>
2021-07-08 09:58:32 -0300

    add user-events

--- src/components/Form.test.js
+++ src/components/Form.test.js
@@ -1,13 +1,14 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 import Form from "./Form";
 
 test("submitting the form calls onSubmit with value and coin", () => {
   const handleSubmit = jest.fn();
   render(<Form onSubmit={handleSubmit} />);
 
-  screen.getByLabelText(/valor/i).value = "3000";
-  screen.getByLabelText(/moeda/i).value = "USD";
-  fireEvent.click(screen.getByRole("button", { name: /calcular/i }));
+  userEvent.type(screen.getByLabelText(/valor/i), "3000");
+  userEvent.selectOptions(screen.getByLabelText(/moeda/i), ["USD"]);
+  userEvent.click(screen.getByRole("button", { name: /calcular/i }));
 
   expect(handleSubmit).toHaveBeenCalledWith({
     value: "3000",
